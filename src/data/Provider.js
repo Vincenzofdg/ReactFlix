16f8d8a8c43aa941739ab32c5057a315d36d0393
Vincenzo F. Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2023-02-17 13:10:41 -0300

    feat: Setting Up

---
+++ src/data/Provider.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Context from "./Context";
+
+function Provider({ children }) {
+  const [test, setTest] = useState("test");
+
+  const data = {
+    test,
+    setTest,
+  };
+
+  return <Context.Provider value={data}>{children}</Context.Provider>;
+}
+
+export default Provider;
